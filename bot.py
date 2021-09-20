6e7ed55ea6a7e81a86ce59e7ba520e8d99877190
Sebastian <shadow.smoke7@gmail.com>
2021-09-20 17:54:29 +0200

    Create SlackBot class
    Working on classes connecting

---
+++ bot.py
@@ -0,0 +1,32 @@
+import slack
+from slack.errors import SlackApiError
+import os
+BOT_TOKEN =
+SLACK_TOKEN =
+
+class SlackBot:
+    def __init__(self):
+        self.bot_token = BOT_TOKEN
+        self.slack_token = SLACK_TOKEN
+        self.client = slack.WebClient(token = self.slack_token)
+
+
+    def get_users_list(self):
+        response = (self.client.users_list(token=BOT_TOKEN))
+        users_list = response.get('members')
+        print(users_list)
+        for user in users_list:
+            user_name = user.get('name')
+            user_id = user.get("id")
+            print(user_name)
+            print(user_id)
+
+
+    def send_message(self, channel):
+        try:
+            self.client.chat_postMessage(channel="U02E3T7JMMH",
+                                    text="Cześć! Ostatni call już za Tobą. Daj znać @Kacper Garbaciński - Devs Mentoring, jeżeli planujesz nie przedłużać abonamentu ",
+                                    token="xoxb-1591793740982-2503927576400-Kk7j2xrQAKKwLpADfXQKvXyf")
+        except SlackApiError as e:
+            assert e.response['error']
+
