be5ad3bbd890f237b1ebbc8104cd013e36fcbf2d
Sebastian <shadow.smoke7@gmail.com>
2021-09-21 17:20:00 +0200

    Get list of users id and send messages

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
+from typing import List
+
 import slack
 from slack.errors import SlackApiError
-import os
-BOT_TOKEN =
-SLACK_TOKEN =
+
+BOT_TOKEN = "xoxb-1591793740982-2503927576400-Kk7j2xrQAKKwLpADfXQKvXyf"
+SLACK_TOKEN = "xoxp-1591793740982-2479925633731-2480302224514-7fb52577608bd9ed4e381562deb41478"
 
 
 class SlackBot:
@@ -12,22 +14,24 @@ class SlackBot:
         self.client = slack.WebClient(token = self.slack_token)
 
 
-    def get_users_list(self):
+    def get_users_list(self, list_of_students:list) -> List:
+
         response = (self.client.users_list(token=BOT_TOKEN))
         users_list = response.get('members')
-        print(users_list)
+        list_of_id = []
         for user in users_list:
-            user_name = user.get('name')
-            user_id = user.get("id")
-            print(user_name)
-            print(user_id)
+            user_name = user.get('real_name')
+            if user_name in list_of_students:
+                user_id = user.get("id")
+                list_of_id.append(user_id)
+        return list_of_id
 
-
-    def send_message(self, channel):
-        try:
-            self.client.chat_postMessage(channel="U02E3T7JMMH",
+    def send_message(self, users_list:list):
+        for user in users_list:
+            try:
+                self.client.chat_postMessage(channel=user,
                                     text="Cześć! Ostatni call już za Tobą. Daj znać @Kacper Garbaciński - Devs Mentoring, jeżeli planujesz nie przedłużać abonamentu ",
                                     token=self.bot_token)
-        except SlackApiError as e:
-            assert e.response['error']
+            except SlackApiError as e:
+                assert e.response['error']
 
