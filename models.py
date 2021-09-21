28d3b02fa819e17ef22b5d7bae7048441202ddc1
Sebastian <shadow.smoke7@gmail.com>
2021-09-21 17:25:50 +0200

    Import db from main

--- models.py
+++ models.py
@@ -1,13 +1,12 @@
-from database import db
-from sqlalchemy import Column, Integer, String, Date, Boolean
+from main import db
+from sqlalchemy import Column, Integer, String
 
 
 class Student(db.base):
     __tablename__ = 'student'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    #date = Column(Date)
     last_month = Column(String)
 
-    def __str__(self):
-        return f'{self.name}, last message {self.date}'
+    def __str__(self) -> str:
+        return f'{self.name}, last message {self.last_month}'
