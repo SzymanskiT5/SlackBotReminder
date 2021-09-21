eec7fdda9a2a868b386fc726b2fa19a0b4ed1eb6
Sebastian <shadow.smoke7@gmail.com>
2021-09-21 17:26:39 +0200

    Connect all classes

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
+
 from bot import SlackBot
 from database import Database
+
 
 db = Database()
 
 
-def main():
+def main() -> None:
     from excel import SheetScraper
+    from models import Student
+    Student.__table__.create(bind=db.engine, checkfirst=True)
     bot = SlackBot()
-
-    db.config_db()
     loc = "/home/sebastian/Pulpit/Students Timesheet.xlsx"
     scraper = SheetScraper(loc)
     scraper.iterate_rows()
-    bot.get_users_list()
-
-
-
-
-
-
-
-
-
+    list_of_students = scraper.list_of_students
+    if list_of_students:
+        list_of_id = bot.get_users_list(list_of_students)
+        bot.send_message(list_of_id)
+    else:
+        print("Everything is up to date")
 
 if __name__ == "__main__":
     main()
