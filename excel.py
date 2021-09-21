817c42fd52511d802869fcb8e95bf5eda746532b
Sebastian <shadow.smoke7@gmail.com>
2021-09-21 17:24:38 +0200

    Add list_of students_field, refactor

--- excel.py
+++ excel.py
@@ -1,23 +1,23 @@
 from openpyxl import load_workbook
 import re
-# from database import Student
+from openpyxl.worksheet.worksheet import Worksheet
 from constans import DATE_REGEX, MONTH_REGEX
 from main import db
 from models import Student
-# from database import db
 
 
 class SheetScraper:
     def __init__(self, localization):
         self.localization = localization
         self.ws = self.open_workbook()
+        self.list_of_students = []
 
-    def open_workbook(self):
+    def open_workbook(self) -> Worksheet:
         wb = load_workbook(self.localization)
         ws = wb.active
         return ws
 
-    def iterate_rows(self):
+    def iterate_rows(self) -> None:
         for idx, row in enumerate(self.ws.iter_rows(min_row=7)):
             start_index = 5
             if re.match(DATE_REGEX, str(row[start_index].value)):
@@ -28,17 +28,16 @@ class SheetScraper:
                 if curr_month:
                     self.check_if_student_exists(name, curr_month)
 
-
-    def find_last_meeting(self, row, start_index):
+    def find_last_meeting(self, row, start_index) -> int:
         diff = 4
         while row[start_index].value != None:
-           start_index += diff
+            start_index += diff
         start_index -= diff
         last_fourth_meeting = row[start_index].value
         print(last_fourth_meeting)
         return start_index
 
-    def check_neighbouring_meetings(self,row, index, idx):
+    def check_neighbouring_meetings(self, row, index, idx) -> str:
         if not re.match(DATE_REGEX, str(row[index + 1].value)) and re.match(DATE_REGEX, str(row[index - 1].value)):
             while not re.search(MONTH_REGEX, str(self.ws.cell(row=idx + 7, column=index - 1).value)):
                 idx -= 1
@@ -46,17 +45,14 @@ class SheetScraper:
             print(curr_month)
             return curr_month
 
-    def check_if_student_exists(self, name, curr_month):
-        student = db.session.query(Student).filter_by(name = name).first()
+    def check_if_student_exists(self, name, curr_month) -> None:
+        student = db.session.query(Student).filter_by(name=name).first()
 
         if not student:
             student = Student(name=name, last_month=curr_month)
             db.session.add(student)
             db.session.commit()
-            print('SEND')
+            self.list_of_students.append(name)
 
-        elif student.last_month == curr_month:
-            print("NOT SEND")
-
-        else:
-            print("SEND")
+        elif student.last_month != curr_month:
+            self.list_of_students.append(name)
