87a54e6f5f17519519248c3fd74c1f5991c7fbb6
Sebastian <shadow.smoke7@gmail.com>
2021-09-21 17:23:21 +0200

    Delete db object in 22 line

--- database.py
+++ database.py
@@ -1,22 +1,21 @@
-import datetime
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker, Session
+from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy_utils import database_exists, create_database
-from sqlalchemy import Column, Integer, String, Date
+
+
 
 class Database:
     def __init__(self):
         self.session = None
+        self.engine = create_engine('sqlite:///bot_base.db', echo=False)
         self.base = declarative_base()
         self.config_db()
 
-    def config_db(self):
-        engine = create_engine(f'sqlite:///bot_base.db', echo=False)
-        Session = sessionmaker(bind=engine)
-        Session.configure(bind=engine)
+    def config_db(self) -> None:
+        if not database_exists(self.engine.url):
+            create_database(self.engine.url)
+        Session = sessionmaker(bind=self.engine)
+        Session.configure(bind=self.engine)
         self.session = Session()
-        self.base.metadata.create_all(engine, self.base.metadata.tables.values())
-        if not database_exists(engine.url):
-            create_database(engine.url)
 
-db = Database()
+
